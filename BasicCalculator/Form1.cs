f63f2152a6353929260dce05ca01568db3af9195
Philippe Rota <philippe.rota@outlook.it>
2020-04-19 11:55:24 +0200

    Declaring methods and functions
    -operators methods
    -numbers methods
    -clearing methods
    Equations functions,and delete functions

--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -36,8 +36,12 @@ namespace BasicCalculator
         #region Clearing methods
         private void DelBtn_Click(object sender, EventArgs e)
         {
+            Delete();
+            FocusInputText();
 
         }
+
+     
         private void CButton_Click(object sender, EventArgs e)
         {
 
@@ -53,33 +57,37 @@ namespace BasicCalculator
         private void button3_Click(object sender, EventArgs e)
         {
             InsertTextValue("7");
+            FocusInputText();
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
             InsertTextValue("8");
+            FocusInputText();
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
             InsertTextValue("9");
+            FocusInputText();
         }
-
-      
 
         private void btn4_Click(object sender, EventArgs e)
         {
             InsertTextValue("4");
+            FocusInputText();
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
             InsertTextValue("5");
+            FocusInputText();
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
             InsertTextValue("6");
+            FocusInputText();
         }
 
         
@@ -87,34 +95,36 @@ namespace BasicCalculator
         private void btn1_Click(object sender, EventArgs e)
         {
             InsertTextValue("1");
+            FocusInputText();
         }
         
 
         private void btn2_Click(object sender, EventArgs e)
         {
             InsertTextValue("2");
+            FocusInputText();
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
             InsertTextValue("3");
+            FocusInputText();
         }
         private void btn0_Click(object sender, EventArgs e)
         {
             InsertTextValue("0");
+            FocusInputText();
 
         }
 
        
         private void Period_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue(".");
+            FocusInputText();
         }
 
-        private void Equal_Click(object sender, EventArgs e)
-        {
-
-        }
+       
 
         #endregion
         
@@ -122,21 +132,31 @@ namespace BasicCalculator
 
         private void Add_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("+");
+            FocusInputText();
         }
         private void Subtract_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("-");
+            FocusInputText();
         }
         private void Multiply_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("*");
+            FocusInputText();
         }
         private void Divide_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("/");
+            FocusInputText();
+        }
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+            FocusInputText();
         }
 
+      
         #endregion
 
         #region Private helpers
@@ -146,8 +166,38 @@ namespace BasicCalculator
         }
         private void InsertTextValue(string v)
         {
+            //track the selection start
+            var selectionStart = this.UserInputText.SelectionStart;
+            //set the new text
             this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart,v);
+            //restore the selection start
+            this.UserInputText.SelectionStart = selectionStart + v.Length;
+            //set the selection lenght to 0
+            this.UserInputText.SelectionLength = 0;
         }
+        private void CalculateEquation()
+        {
+            FocusInputText();
+        }
+        private void Delete()
+        {
+            
+            //if we don't have a value to delete -> return nothing
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+            
+            //remeber the selection
+            var selectionStart = this.UserInputText.SelectionStart;
+            //delete the character to the right of the selection
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+
+            //restore the selection start
+            this.UserInputText.SelectionStart = selectionStart;
+            
+            //set the selection lenght to 0
+            this.UserInputText.SelectionLength = 0;
+        }
+
 
         #endregion
     }
